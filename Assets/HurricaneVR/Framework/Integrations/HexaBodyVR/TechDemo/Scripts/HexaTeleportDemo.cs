8a0f3ede9cd872800c27fcb11758b5f1a4d5580c
Jack Myrick <jm247334@falmouth.ac.uk>
2024-03-02 17:44:49 +0000

    Migrating Assets: HurricaneVR & Materials for Programmers

---
+++ Assets/HurricaneVR/Framework/Integrations/HexaBodyVR/TechDemo/Scripts/HexaTeleportDemo.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using HexabodyVR.PlayerController;
+using HurricaneVR.Framework.Core.Grabbers;
+using UnityEngine;
+
+public class HexaTeleportDemo : MonoBehaviour
+{
+    public HexaBodyPlayer4 OculusHexabody;
+    public HexaBodyPlayer4 OpenXRHexabody;
+    private HexaBodyPlayer4 Hexa;
+    public Transform TeleportPosition;
+    public List<HVRHandGrabber> Hands = new List<HVRHandGrabber>();
+
+    public bool Teleport;
+
+    private void Start()
+    {
+        if (OculusHexabody.isActiveAndEnabled)
+        {
+            Hexa = OculusHexabody;
+        }
+        else if (OpenXRHexabody.isActiveAndEnabled)
+        {
+            Hexa = OpenXRHexabody;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Teleport)
+        {
+            Teleport = false;
+
+            //MoveToPosition is based on the ball bottom, so we capture the ball bottom before teleporting
+            var start = Hexa.LocoBall.transform.position + Vector3.down * Hexa.LocoCollider.radius;
+            Hexa.MoveToPosition(TeleportPosition.position);
+            var delta = TeleportPosition.position - start;
+
+            for (int i = 0; i < Hands.Count; i++)
+            {
+                var hand = Hands[i];
+                if (hand && hand.HeldObject && hand.HeldObject.Rigidbody)
+                {
+                    var rb = hand.HeldObject.Rigidbody;
+                    rb.transform.position += delta;
+                    rb.position += delta;
+                }
+            }
+        }
+    }
+
+    public void TeleportNow()
+    {
+        Teleport = true;
+    }
+}
---
+++ Assets/HurricaneVR/TechDemo/Scripts/DemoPowerUnit.cs
@@ -0,0 +1,86 @@
+﻿using HurricaneVR.Framework.Components;
+using HurricaneVR.Framework.Core;
+using HurricaneVR.Framework.Core.Grabbers;
+using HurricaneVR.Framework.Core.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HurricaneVR.TechDemo.Scripts
+{
+    public class DemoPowerUnit : MonoBehaviour
+    {
+        public UnityEvent PoweredUp = new UnityEvent();
+        public UnityEvent PoweredDown = new UnityEvent();
+        public UnityEvent CellsActivated = new UnityEvent();
+
+        public HVRSocket LeftSocket;
+        public HVRSocket RightSocket;
+
+        public MeshRenderer LeftLight;
+        public MeshRenderer RightLight;
+
+        public HVRRotationTracker LeverRotation;
+        public float RequiredAngle = 175f;
+
+        public AudioClip SFXPoweredOn;
+    
+        public int RequiredCells = 2;
+        public int SocketedCellCount;
+        public bool Engaged;
+        private bool OffPlayed;
+        private bool CellsPlayed;
+
+        public Material OnMaterial;
+
+        public bool IsPoweredUp => SocketedCellCount == RequiredCells;
+
+        void Start()
+        {
+            LeftSocket.Grabbed.AddListener(OnLeftSocketGrabbed);
+            RightSocket.Grabbed.AddListener(OnRightSocketGrabbed);
+        }
+
+        private void OnRightSocketGrabbed(HVRGrabberBase arg0, HVRGrabbable arg1)
+        {
+            SocketedCellCount++;
+            var mats = RightLight.materials;
+            mats[0] = OnMaterial;
+            RightLight.materials = mats;
+        }
+
+        private void OnLeftSocketGrabbed(HVRGrabberBase arg0, HVRGrabbable arg1)
+        {
+            SocketedCellCount++;
+            var mats = LeftLight.materials;
+            mats[0] = OnMaterial;
+            LeftLight.materials = mats;
+        }
+
+        void Update()
+        {
+
+            if (LeverRotation.Angle > RequiredAngle && IsPoweredUp && !Engaged)
+            {
+                if(SFXPlayer.Instance) SFXPlayer.Instance.PlaySFX(SFXPoweredOn, LeftSocket.transform.position);
+                Engaged = true;
+                PoweredUp.Invoke();
+                Debug.Log($"poweredup!");
+                OffPlayed = false;
+                CellsPlayed = false;
+            }
+
+            else if (!Engaged && !OffPlayed)
+            {
+                PoweredDown.Invoke();
+                OffPlayed = true;
+                Engaged = false;
+            }
+
+            else if (SocketedCellCount == RequiredCells && !Engaged && !CellsPlayed)
+            {
+                CellsActivated.Invoke();
+                CellsPlayed = true;
+            }
+        }
+    }
+}
